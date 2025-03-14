3caac611a20ccdf93b7ec6568225a8fa2582c233
roboticseagles358 <roboticseagles358@gmail.com>
2025-03-13 21:05:37 -0400

    small elevator changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,21 +6,27 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
 
+import java.util.Set;
+
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.DeferredCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
-
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.subsystems.ElevatorSubsytem;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.ElevatorSubsytem;
 
 public class RobotContainer {
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
@@ -36,6 +42,9 @@ public class RobotContainer {
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
     private final CommandXboxController joystick = new CommandXboxController(0);
+    private final CommandXboxController operatorController = new CommandXboxController(1);
+    @Logged(name = "Elevator")
+    private final ElevatorSubsytem elevator = new ElevatorSubsytem();
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
@@ -43,6 +52,7 @@ public class RobotContainer {
     private final SendableChooser<Command> autoChooser;
 
     public RobotContainer() {
+
         //configures dashboard to have an autonomose mode chooser
         autoChooser = AutoBuilder.buildAutoChooser("Auto Chooser");
         Shuffleboard.getTab("Auto Chooser").add(autoChooser);
@@ -83,4 +93,56 @@ public class RobotContainer {
 
         drivetrain.registerTelemetry(logger::telemeterize);
     }
+
+    private void configureElevatorBindings() {
+        
+        elevator.setDefaultCommand(elevator.holdPosition());
+        // Elevator L4
+        operatorController
+            .b()
+            .and(outtakeLaserBroken)
+            .or(operatorController.povLeft().and(operatorController.b()))
+            .onTrue(elevator.moveToPosition(ElevatorConstants.L4Height));
+
+         // elevator L3
+            .y()
+            .and(outtakeLaserBroken)
+            .or(operatorController.povLeft().and(operatorController.y()))
+            .onTrue(elevator.moveToPosition(ElevatorConstants.L3Height));
+
+        // elevator L2
+        operatorController
+            .x()
+            .and(outtakeLaserBroken)
+            .or(operatorController.povLeft().and(operatorController.x()))
+            .onTrue(elevator.moveToPosition(ElevatorConstants.L2Height));
+        // elevator down height
+        operatorController.a().onTrue(elevator.downPosition());
+
+        // home elevator
+        operatorController.start().and(operatorController.back()).onTrue(elevator.homeElevator());
+        // coral in the way add
+        operatorController
+            .povRight()
+            .onTrue(
+                new DeferredCommand(
+                    () -> {
+                    double newTarget =
+                          Units.inchesToMeters(
+                              elevator.getPositionInches() + ElevatorConstants.coralInTheWayAdd);
+                    return elevator.moveToPosition(newTarget);
+                    },
+                    Set.of(elevator)));
+         // elevator manual down
+        operatorController
+            .povDown()
+            .whileTrue(elevator.downSpeed(0.1))
+            .onFalse(elevator.runOnce(() -> elevator.stopElevator()));
+        // elevator manual up
+        operatorController
+            .povUp()
+            .whileTrue(elevator.upSpeed(0.1))
+            .onFalse(elevator.runOnce(() -> elevator.stopElevator()));
+  }
+
 }
